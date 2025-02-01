bcf9c3a2fd19ad7cf0c58a44b1fd284fee377032
abhinay <abhinay212@gmail.com>
2025-02-01 19:04:09 +0530

    feat: Improve note title and content hint visibility
    This commit refines the behavior of the hint visibility in the note title and content text fields.
    
    - Updated `TransparentHintTextField` to show hint when `isHintVisible` is true.
    - Updated `AddEditNoteViewModel` to correctly hide or show the hint based on the `isFocused` state and whether the text field is blank.

--- CleanArchitectureNoteApp/app/src/main/java/com/abhinay/cleanarchitecturenoteapp/feature_note/presentation/add_edit_note/components/TransparentHintTextField.kt
+++ CleanArchitectureNoteApp/app/src/main/java/com/abhinay/cleanarchitecturenoteapp/feature_note/presentation/add_edit_note/components/TransparentHintTextField.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 
 
 /**
@@ -50,16 +52,12 @@ fun TransparentHintTextField(
     modifier: Modifier = Modifier,
     isHintVisible: Boolean = true,
     onValueChange: (String) -> Unit,
-    textStyle: androidx.compose.ui.text.TextStyle = androidx.compose.ui.text.TextStyle(),
+    textStyle: TextStyle = TextStyle(),
     singleLine: Boolean = false,
-    onFocusChange: (androidx.compose.ui.focus.FocusState) -> Unit
-
-
+    onFocusChange: (FocusState) -> Unit
 ) {
-
     Box(
         modifier = modifier
-
     ) {
         BasicTextField(
             value = text,
@@ -72,9 +70,8 @@ fun TransparentHintTextField(
                     onFocusChange(it)
                 }
         )
-        if (!isHintVisible) {
+        if(isHintVisible) {
             Text(text = hint, style = textStyle, color = Color.DarkGray)
         }
-
     }
 }
