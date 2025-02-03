4c2f6c91dee17e70c65caac4784c71b74d0e6764
abhinay <abhinay212@gmail.com>
2025-02-04 00:20:07 +0530

    feat: Implement GetNotes use case tests and FakeNoteRepository
    This commit introduces comprehensive unit tests for the `GetNotes` use case and a `FakeNoteRepository` for testing purposes.
    
    -   Added `GetNotesTest` to verify the behavior of the `GetNotes` use case with different ordering criteria (title, date, and color, both ascending and descending).
    -   Created `FakeNoteRepository` to simulate a real repository for testing.
    -   Updated the project to include truth library for the assertions.

---
+++ CleanArchitectureNoteApp/app/src/test/java/com/abhinay/cleanarchitecturenoteapp/feature_note/data/repository/FakeNoteRepository.kt
@@ -0,0 +1,33 @@
+package com.abhinay.cleanarchitecturenoteapp.feature_note.data.repository
+
+import com.abhinay.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import com.abhinay.cleanarchitecturenoteapp.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+
+/**
+ * Created by Abhinay on 03/02/25.
+ *
+ *
+ */
+class FakeNoteRepository: NoteRepository {
+
+    private val notes = mutableListOf<Note>()
+
+    override fun getNotes(): Flow<List<Note>> {
+        return flow { emit(notes) }
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        return notes.find { it.id == id }
+    }
+
+    override suspend fun insertNote(note: Note) {
+        notes.add(note)
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        notes.remove(note)
+    }
+}
